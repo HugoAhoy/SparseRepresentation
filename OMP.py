72bc8e617568fa51171bdd7afb0681bf9b4b7d66
HugoAhoy <zhjd3302@outlook.com>
2019-12-17 13:03:22 +0800

    complete coursework

--- OMP.py
+++ OMP.py
@@ -1,47 +1,69 @@
 import numpy as np
-import copy
+import os
 
+# 最小二乘
 def LSFit(A, y):
     return np.matmul(np.linalg.inv(np.matmul(A.T, A)), np.matmul(A.T, y))
 
-def findPos(vec):
-    idx = 0
-    for i in range(len(vec)):
-        if(abs(vec[i]) > abs(vec[idx])):
-            idx = i
-    return idx
+# 用伪逆最小二范数
+def LSwithPinv(A, y):
+    return np.matmul(np.linalg.pinv(np.matmul(A.T, A)), np.matmul(A.T, y))
 
+# OMP算法，A为字典，y为待表达信号，iter_num为迭代次数
 def OMP(A, y, iter_num):
     k = 0
-    # 深拷贝
-    res = copy.deepcopy(y)
-    Allbase = copy.deepcopy(A)
-    
-    # 基的集合
-    base = np.zeros([A.shape[0],0])
+    # 初始化残差
+    res = np.array(y, dtype=np.float64)
+    # 初始化所有的基
+    Allbase = np.array(A, dtype=np.float64)
+
+    # 选择的基的集合
+    base = np.zeros([A.shape[0],0], dtype=np.float64)
+
+    # 用于记录选择的基在所有基中的位置
     pos = []
+
+    # 迭代
     while k < iter_num:
         # 残差在各个基上的投影
-        proj = np.matmul(Allbase.T, res)
+        proj = np.matmul(res.T,Allbase)
 
         # 获得投影长度最大的基的索引并加入基的集合
-        idx = findPos(proj)
+        # idx = findPos(proj)
+        idx = np.argmax(abs(proj))
         base = np.column_stack((base, Allbase[:, idx]))
         pos.append(idx)
 
-        # 将原来对应的基置0,这样在选择基的时候就相当于删除了
+        # 将原来对应的基置0,这样在选择基的时候就相当于删除了,不会被重复选择
         Allbase[:, idx] = 0
         k = k + 1
 
-        # 最小二乘重新计算表达和残差
-        x = LSFit(base, y)
+        # 最小二乘重新计算表达
+        # x = LSFit(base, y)
+
+        # 伪逆计算最小二范数重新计算表达
+        x = LSwithPinv(base, y)
+
+        # 计算残差
         res = y - np.matmul(base, x)
-    signal = np.zeros(A.shape[1])
-    x = LSFit(base, y)
+        # print("x\n",x)
+        # print("base\n",base)
+        # print("res\n", res)
+        # os.system("pause")
+        print(k)
+    
+    # 信号构成
+    signal = np.zeros(A.shape[1], dtype=np.float64)
+    
+    # 计算最终表达
+    x = LSwithPinv(base, y)
+
+    # 生成最终稀疏信号
     for i in range(len(pos)):
         signal[pos[i]] = x[i]
     return signal
 
+# 单元测试
 if __name__ == "__main__":
     A = np.array([[-0.707, 0.8, 0],[0.707, 0.6, -1]])
     y = np.array([[1.65],[-0.25]])
--- main.py
+++ main.py
@@ -6,38 +6,53 @@ from PIL import Image
 import numpy as np
 import re
 import OMP
+from matplotlib import pyplot as plt
 # from sklearn.linear_model import OrthogonalMatchingPursuit
 
 # 设定参数
+# 下采样压缩后的列数
 resizeCol = 40
+# 下采样压缩后的行数
 resizeRow = 55
 
-# 从文件中根据文件名正则匹配得到训练数据
-def getTrain(files):
-    result = []
+# 从文件中根据文件名正则匹配得到训练数据和测试数据
+def DivTrainTest(files):
+    trains = []
+    tests = []
     expr = re.compile("\S-\d*-(\d+?)\.pgm")
     for i in files:
         num = int(expr.findall(i)[0])
         if num < 14:
             if num <= 7:
-                result.append(i)
+                trains.append(i)
+            else:
+                tests.append(i)
         else:
             if num <= 20:
-                result.append(i)
-    return result
+                trains.append(i)
+            else:
+                tests.append(i)
+    return trains, tests
 
 # 读取图像文件
 def read_img(FilePath, downSample = True):
-    im = Image.open(FilePath)    # 打开文件
+    # 打开文件
+    im = Image.open(FilePath)
+    # 判断是否下采样压缩
     if downSample:
         im = im.resize((resizeCol,resizeRow))
 
     # 默认为uint8,归一化时会溢出,所以需要更改类型
     im = np.array(im).astype(int).reshape(-1, 1)
+
+    # # 归一化系数
     # division = math.sqrt(sum(im*im))
+    # # 归一化
     # im = im / division
-    # 返回归一化结果和归一化系数
+    # # 返回归一化结果和归一化系数
     # return im, division
+
+    # 不归一化
     return im
 
 # 读入训练数据
@@ -45,10 +60,13 @@ def readTrainData(DirPath, trainNum, trainTotal):
     A = []
     i = 0
     for root, dirs, files in os.walk(DirPath):
-        files = getTrain(files)
-        for f in files:
+        TrainFiles, TestFiles = DivTrainTest(files)
+        for f in TrainFiles:
             if i < trainNum:
+                # 归一化
                 # pic, div = read_img(os.path.join(root,f))
+
+                # 不归一
                 pic = read_img(os.path.join(root,f))
 
                 A.append(pic)
@@ -57,6 +75,19 @@ def readTrainData(DirPath, trainNum, trainTotal):
                 i = 0
         return np.concatenate(A, axis=1)
 
+# 获取测试数据
+def getTestData(DirPath, testNum, testTotal):
+    A = []
+    i = 0
+    for root, dirs, files in os.walk(DirPath):
+        TrainFiles, TestFiles = DivTrainTest(files)
+        for f in TestFiles:
+            if i < testNum:
+                A.append(os.path.join(root,f))
+            i = i+1
+            if i >= testTotal:
+                i = 0
+        return A
 
 # 通过读入的训练数据构建字典
 def getDictionary(TrainData):
@@ -65,14 +96,50 @@ def getDictionary(TrainData):
 
 # 构建字典
 s = time.time()
-TrainData = readTrainData(os.path.join(os.getcwd(),"test"), 10, 14)
+TrainData = readTrainData(os.path.join(os.getcwd(),"test"), 7, 14)
 A = getDictionary(TrainData)
 e = time.time()
-print(A.shape)
-print("build dic cost",e-s, " s")
+print("字典大小为",A.shape)
+print("构建字典耗时",e-s, " s")
+
+# TestData = getTestData(os.path.join(os.getcwd(),"test"), 4, 12)
+
+# 测试
+for i in TestData:
+    # y, div = read_img(i)
+    # 不归一化
+    y= read_img(i)
+    # Self-Written OMP
+    s = time.time()
+    result = OMP.OMP(A, y, A.shape[0])
+    e = time.time()
+    print("解决该问题耗时",e-s, " s")
+    # 误差系数
+    e = result[-A.shape[0]:].reshape(-1,1)
+    # 表达系数
+    coe = result[0:-A.shape[0]].reshape(-1,1)
+    print("表达系数:\n",coe)
+    print("误差系数:\n",e)
+    # # 归一化
+    # # 原图
+    # rawImg = Image.fromarray(np.uint8((y*div).reshape((55,40)))).convert("RGB")
+    # # 去遮挡恢复的图片
+    # recoverImg = Image.fromarray(np.uint8(((y - e)*div).reshape((55,40)))).convert("RGB")
+    # 不归一化
+    # 原图
+    rawImg = Image.fromarray(np.uint8((y).reshape((55,40)))).convert("RGB")
+    # 去遮挡恢复的图片
+    recoverImg = Image.fromarray(np.uint8((y - e).reshape((55,40)))).convert("RGB")
+
+    plt.subplot(1,2,1)
+    plt.imshow(rawImg)
+    plt.subplot(1,2,2)
+    plt.imshow(recoverImg)
+    plt.show()
+    # Image.fromarray(np.uint8(y - e).reshape((resizeRow,resizeCol))).show()
 
 # y, div = read_img("d:\GitRepository\SparseRepresentation\\test\m-001-13.pgm")
-y = read_img("d:\GitRepository\SparseRepresentation\\test\m-001-13.pgm")
+# y = read_img("d:\GitRepository\SparseRepresentation\\test\m-001-13.pgm")
 
 # model = OrthogonalMatchingPursuit(n_nonzero_coefs=A.shape[1])
 
@@ -90,14 +157,14 @@ y = read_img("d:\GitRepository\SparseRepresentation\\test\m-001-13.pgm")
 # Image.fromarray(np.uint8((y - e).reshape((resizeRow,resizeCol)))).show()
 
 
-# Self-Written OMP
-s = time.time()
-result = OMP.OMP(A, y, A.shape[0])
-e = time.time()
-print("Solving the problem cost",e-s, " s")
-e = result[-A.shape[0]:].reshape(-1,1)
-coe = result[0:-A.shape[0]].reshape(-1,1)
-print(coe)
-print(e)
-# Image.fromarray(np.uint8(((y - e)*div).reshape((55,40)))).show()
-Image.fromarray(np.uint8(y - e).reshape((resizeRow,resizeCol))).show()
+# # Self-Written OMP
+# s = time.time()
+# result = OMP.OMP(A, y, A.shape[0])
+# e = time.time()
+# print("Solving the problem cost",e-s, " s")
+# e = result[-A.shape[0]:].reshape(-1,1)
+# coe = result[0:-A.shape[0]].reshape(-1,1)
+# print(coe)
+# print(e)
+# # Image.fromarray(np.uint8(((y - e)*div).reshape((55,40)))).show()
+# Image.fromarray(np.uint8(y - e).reshape((resizeRow,resizeCol))).show()
